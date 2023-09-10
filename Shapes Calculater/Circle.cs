9d8bcf23cd79b6df200f1ee903b62f750f1a6bb6
dyandilthusha <ryandilthusha@gmail.com>
2023-09-11 03:04:01 +0530

    Add project files.

---
+++ Shapes Calculater/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Shapes
+{
+    class Circle
+    {
+        public double radius;
+
+
+
+        public Circle(double r)
+        {
+            this.radius = r;
+        }
+
+
+
+        public void calarea()
+        {
+            double area = 3.14 * this.radius * this.radius;
+            Console.WriteLine("Area of Circle = " + area);
+        }
+
+
+
+        public void calperimeter()
+        {
+            double peri = 2 * 3.14 * this.radius;
+            Console.WriteLine("Perimeter of Circle = " + peri);
+        }
+    }
+}
---
+++ Shapes Calculater/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Shape Name : ");
+            String sname = Console.ReadLine();
+
+
+
+            Console.WriteLine("Which Operation : ");
+            String op = Console.ReadLine();
+
+
+
+            if (sname == "Circle")
+            {
+                Console.WriteLine("Enter Radius : ");
+                double rad = double.Parse(Console.ReadLine());
+
+
+
+                Circle c = new Circle(rad);
+
+                c.calarea();
+                c.calperimeter();
+
+
+            }
+            else if (sname == "Square")
+            {
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
